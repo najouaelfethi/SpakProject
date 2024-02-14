c78583d1c19fea10fd5b88bbe97a1465c9d5ceb7
NajouaELFETHI <elfethinajwa@outlook.fr>
2024-02-14 15:36:10 +0100

    update

---
+++ sparkupdate2.py
@@ -0,0 +1,60 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import IntegerType
+from pyspark import SparkContext
+
+def create_spark_session():
+    return SparkSession.builder.appName("firstApp").getOrCreate()
+
+def load_data(spark, file_path):
+    return spark.read.csv(file_path, header=True)
+
+def clean_data(data):
+    data = data.drop("Column1", "company_id", "posted_day_ago")
+    data = data.withColumn("alumni", F.regexp_extract("alumni", r'(\d+)', 1).cast(IntegerType())) \
+        .withColumn("linkedin_followers", F.regexp_replace("linkedin_followers", r' followers', '').cast(IntegerType())) \
+        .withColumn("no_of_application", F.col("no_of_application").cast('int'))
+    data = data.na.drop()
+    return data
+
+def analyse_data(data):
+    data.show(1)
+    data.printSchema()
+    summary_stats = data.describe(["no_of_employ", "no_of_application"])
+    return summary_stats.show()
+
+def transformations_rdd(data_rdd):
+    sc = SparkContext.getOrCreate()
+    data_rdd = sc.parallelize(data.rdd.collect(),5)
+    partitions = data_rdd.getNumPartitions()
+    print("Initial partition count:", partitions, "partitions")
+    rdd1 = data_rdd.map(lambda x: (x[1], x[2], x[3]))
+    rdd2 = rdd1.filter(lambda x: x[2] == "India")
+    return rdd1,rdd2
+    
+def actions_rdd(data_rdd,rdd2):
+    first_element = data_rdd.first()
+    take_data = data_rdd.take(5)
+    count_rdd = data_rdd.count()
+
+    print("First element:", first_element)
+    print("Total number of rdd elements:", count_rdd)
+
+    output_path = "hdfs:///user/maria_dev/spark/output"
+    rdd2.saveAsTextFile(output_path)
+
+if __name__ == "__main__":
+    spark_session = create_spark_session()
+
+    file_path = "linkdin_Job_data.csv"
+    data = load_data(spark_session, file_path)
+
+    cleaned_data = clean_data(data)
+    analyse_data(cleaned_data)
+
+    data_rdd = cleaned_data.rdd
+
+    rdd1,rdd2 = transformations_rdd(data_rdd)
+    
+    actions_rdd(data_rdd,rdd2)
+
